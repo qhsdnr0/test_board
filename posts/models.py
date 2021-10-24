a1f39eb07e6313ed47fdbeb467da37051858c98a
qhsdnr0 <qhsdnr0@gmail.com>
2021-10-24 15:33:17 +0900

    Modify: posts.models, users.models

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,17 @@
-from django.db import models
+from django.db              import models
 
 from test_board.core.models import TimeStamp
 
 class Post(TimeStamp):
-    title = models.CharField(max_length=45)
+    title   = models.CharField(max_length=45)
     content = models.TextField(max_length=2000)
 
     class Meta:
         db_table = 'posts'
+
+class Image(models.Model):
+    image_url = models.CharField(max_length=2000)
+    post      = models.ForeignKey('Post', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'images'
--- users/models.py
+++ users/models.py
@@ -1,11 +1,11 @@
-from django.db import models
+from django.db              import models
 
 from test_board.core.models import TimeStamp
 
 class User(TimeStamp):
-    name = models.CharField(max_length=100)
-    account = models.CharField(max_length=45)
-    password = models.CharField(max_length=250)
+    name          = models.CharField(max_length=100)
+    account       = models.CharField(max_length=45)
+    password      = models.CharField(max_length=250)
     date_of_birth = models.DateField(null=True)
 
     class Meta:
